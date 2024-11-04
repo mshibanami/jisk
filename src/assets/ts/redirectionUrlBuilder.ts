8a5eea6658c4e6c33915c625aa028ea93e973f1d
Manabu Nakazawa <mshibanami+git@gmail.com>
2024-11-04 16:11:31 +1100

    Refactoring and fix potential bugs

--- src/assets/ts/redirectionUrlBuilder.ts
+++ src/assets/ts/redirectionUrlBuilder.ts
@@ -3,33 +3,33 @@ import { serviceId } from "./instances";
 export function makeRedirectionUrl({
     baseUrl,
     sourceUrl,
+    serviceId,
     encodesSourceUrl,
     cacheExpiry,
-    serviceId,
     hasHtmlExtension,
     countries
 }: {
     baseUrl: string,
     sourceUrl: string,
-    encodesSourceUrl: boolean,
-    cacheExpiry: number,
     serviceId: serviceId,
-    hasHtmlExtension: boolean,
-    countries: string[]
+    encodesSourceUrl?: boolean,
+    cacheExpiry?: number,
+    hasHtmlExtension?: boolean,
+    countries?: string[]
 }): string | null {
     const resourceName = serviceId + (hasHtmlExtension ? '.html' : '')
     const components = new URL(resourceName, baseUrl)
     if (sourceUrl.length > 0) {
-        if (encodesSourceUrl) {
+        if (encodesSourceUrl === undefined || encodesSourceUrl) {
             components.searchParams.set('url', sourceUrl)
         } else {
             components.search = `url=${sourceUrl}`
         }
     }
-    if (cacheExpiry > 0) {
+    if (cacheExpiry && cacheExpiry > 0) {
         components.searchParams.set('cache_expiry', cacheExpiry.toString())
     }
-    if (countries.length > 0) {
+    if (countries && countries.length > 0) {
         components.searchParams.set('countries', countries.join(','))
     }
     return components.toString()
